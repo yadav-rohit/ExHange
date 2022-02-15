65cc3e8fb79c5699c9c915e5cf7ac99856d2e5e2
yadav-rohit <49914319+yadav-rohit@users.noreply.github.com>
2022-02-15 20:24:23 +0530

    nn

--- Client/pages/index.tsx
+++ Client/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Header from '../Components/Header';
 import Main from '../Components/Main';
 const style = {
-  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#1c0000] text-[#FF0000] select-none flex flex-col justify-between content-center`,
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#260000] text-[#FF0000] select-none flex flex-col justify-between content-center`,
 }
 export default function Home() {
   return (
